e5146c30b7cd1209439ae90beff758e3304e3472
chammmiii <58718071+chammmiii@users.noreply.github.com>
2023-06-13 14:57:20 +0900

    Add files via upload

---
+++ selenium_test.py
@@ -0,0 +1,24 @@
+import streamlit as st
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
+@st.experimental_memo
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
+options = Options()
+options.add_argument('--disable-gpu')
+options.add_argument('--headless')
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
+options.add_argument("--disable-features=NetworkService")
+options.add_argument("--window-size=1920x1080")
+options.add_argument("--disable-features=VizDisplayCompositor")
+
+driver = get_driver()
+driver.get('https://dart.fss.or.kr/dsaf001/main.do?rcpNo=20230530000941')
+
+st.code(driver.page_source)
